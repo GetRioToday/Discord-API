8515c7548e4dcb53dc0e0fec440ddec6efac5e04
GetRioToday <riosoftware@proton.me>
2025-04-18 03:27:02 -0400

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,7 +1,7 @@
 local RPC = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/GetRioToday/Discord-API/refs/heads/main/RPC.lua"))()
 
-local Connection = GetConnectionUrl()
+local Connection = RPC.GetConnectionUrl()
 if Connection then
-	local Success = JoinServer(Connection, "roblox") -- "roblox" is the "https://discord.gg/roblox" vanity server code
+	local Success = RPC.JoinServer(Connection, "roblox") -- "roblox" is the "https://discord.gg/roblox" vanity server code
 	print("Displayed Server Invite:", Success)
 end
