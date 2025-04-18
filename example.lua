4f2a85ff45748540fa7dd07f7e38b0b4ae286df4
GetRioToday <riosoftware@proton.me>
2025-04-18 17:23:53 -0400

    Update example.lua

--- example.lua
+++ example.lua
@@ -2,6 +2,19 @@ local RPC = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/GetR
 
 local Connection = RPC.GetConnectionUrl()
 if Connection then
+	-- Join server example which wraps SendCommand()
 	local Success = RPC.JoinServer(Connection, "roblox") -- "roblox" is the "https://discord.gg/roblox" vanity server code
 	print("Displayed Server Invite:", Success)
+
+	-- Send custom command example using the INVITE_BROWSER command (for more commands and their arguments, see: https://discord.com/developers/docs/topics/rpc#commands-and-events)
+	local Command = {
+		["cmd"] = "INVITE_BROWSER",
+		["nonce"] = game:GetService("HttpService"):GenerateGUID(false),
+		["args"] = {
+			["code"] = "roblox",
+		}
+	}
+
+	local Success = SendCommand(Connection, Command)
+	print("Custom Command Result:", Success)
 end
